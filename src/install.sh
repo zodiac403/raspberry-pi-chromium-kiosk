13c737f56ee825214df3e37264185b46fac2b046
zodiac403 <zodiac403@gmx.de>
2023-09-16 00:08:41 +0200

    Add apt install

--- src/install.sh
+++ src/install.sh
@@ -1,2 +1,18 @@
-echo "configure autostart"
+echo "install xserver and chromium-browser..."
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install --no-install-recommends -y \
+  chromium-browser \
+  x11-xserver-utils \
+  xinit \
+  xserver-xorg-core \
+  xserver-xorg-input-all \
+  xserver-xorg-video-all
+
+echo "copy XServer init configuration..."
+cp xinitrc ~/.xinitrc
+
+echo "configure autostart..."
 cat bash_profile_snippet >> ~/.bash_profile
+
+echo "success"
