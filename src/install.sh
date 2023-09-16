4eb6a9caa686e649300bf12de4ee109f71fb6e49
zodiac403 <zodiac403@gmx.de>
2023-09-16 22:28:41 +0200

    Fine-tune firewall configuration

--- src/install.sh
+++ src/install.sh
@@ -12,8 +12,13 @@ sudo apt-get install --no-install-recommends -y \
 
 echo "configure firewall (block incoming traffic on all ports)..."
 sudo iptables --policy INPUT DROP
+sudo iptables --append INPUT \
+  --match conntrack \
+  --ctstate ESTABLISHED,RELATED \
+  --jump ACCEPT
 sudo iptables --policy FORWARD DROP
 sudo iptables --policy OUTPUT ACCEPT
+sudo iptables --list
 
 echo "copy XServer init configuration..."
 cp xinitrc ~/.xinitrc
