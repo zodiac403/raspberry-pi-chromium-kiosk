3c96bad7df6caae1cf50e0e14d1929deb7d6496b
zodiac403 <zodiac403@gmx.de>
2023-10-12 10:07:42 +0200

    configure firewall (block incoming traffic on all ports)

--- src/install.sh
+++ src/install.sh
@@ -1,4 +1,4 @@
-echo "install xserver and chromium-browser..."
+echo "install xserver and chromium-browser"
 sudo apt update && sudo apt upgrade -y
 
 sudo apt-get install --no-install-recommends -y \
@@ -10,10 +10,24 @@ sudo apt-get install --no-install-recommends -y \
   xserver-xorg-input-all \
   xserver-xorg-video-all
 
-echo "copy XServer init configuration..."
+echo ""
+echo "configure firewall (block incoming traffic on all ports)"
+sudo iptables --policy INPUT DROP
+sudo iptables --append INPUT \
+  --match conntrack \
+  --ctstate ESTABLISHED,RELATED \
+  --jump ACCEPT
+sudo iptables --policy FORWARD DROP
+sudo iptables --policy OUTPUT ACCEPT
+sudo iptables --list
+
+echo ""
+echo "copy XServer init configuration"
 cp xinitrc ~/.xinitrc
 
-echo "configure autostart..."
+echo ""
+echo "configure autostart"
 cat bash_profile_snippet >> ~/.bash_profile
 
+echo ""
 echo "success"
